fcae952837e0db6d93cc69f2088d4bc496c26a61
Jesus Neyra <jesua.park@gmail.com>
2021-11-25 10:58:12 -0300

    Correcion de errores

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ app.use(express.static('public'));
 
 
 //corro el servidor
-app.listen(process.env.PORT || 3000,()=>console.log("Servidor corriendo"));
+app.listen(process.env.PORT || 3000,()=>console.log("Servidor funcionando"));
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/views/home.html');
+    res.sendFile(__dirname + '/views/home.html');
 });
 app.get('/register',(req,res)=>{
-    res.sendFile(__dirname+'/views/register.html');
+    res.sendFile(__dirname + '/views/register.html');
 });
 app.get('/login',(req,res)=>{
-    res.sendFile(__dirname+'/views/login.html');
+    res.sendFile(__dirname + '/views/login.html');
 });
 
